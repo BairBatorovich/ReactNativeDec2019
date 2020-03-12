ee4c93964107c841d5accb0c9304b779baff6bb0
Bair <pinhaev17@gmail.com>
2020-03-12 19:28:28 +0800

    delete vk login package

--- screens/Authorization.js
+++ screens/Authorization.js
@@ -3,26 +3,13 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import { useDispatch } from 'react-redux';
 import { authorization } from '../store/action';
-import VKLogin from 'react-native-vkontakte-login';
 
 export default function Authorization(props) {
     const dispatch = useDispatch();
     const signin = () => {
-        // props.navigation.navigate('General');
         dispatch(authorization(true))
     }
-    const vklogin = async () => {
-        console.log('123')
-        VKLogin.initialize(7344988);
-        const isLoggedIn = await VKLogin.isLoggedIn();
-        const auth = await VKLogin.login(['friends', 'photos', 'email']);
-        console.log(auth);
-    }
-    useEffect(
-        () => {
-            vklogin();
-        },
-        [])
+
     return (
         <View style={styles.authorization}>
             <Text style={styles.txt}>Авторизация</Text>
